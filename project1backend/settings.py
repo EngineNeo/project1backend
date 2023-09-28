34d1bae5acd15dc8c5306c09493cabbfd6278aad
unknown <neomaralit@gmail.com>
2023-09-28 16:15:23 -0400

    Changed database settings

--- project1backend/settings.py
+++ project1backend/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = "project1backend.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sakila',
+        'USER': 'root',
+        'PASSWORD': 'Password123',
+        'HOST': 'localhost',   # Set to your MySQL host.
+        'PORT': '3306',        # Default MySQL port.
     }
 }
 
